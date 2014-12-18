8c0d498213c89f556156d73defba6f6c10766612
Nick <nick@narrowdesign.com>
2014-12-17 21:52:12 -0500

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+document.addEventListener('keypress',function(e){
+  document.getElementById("search").focus();
+})
+// window.addEventListener('resize',function(e){
+//   document.getElementsByTagName('body')[0].style.fontSize = 16*(window.innerWidth * window.innerHeight)/1440000;
+// })
